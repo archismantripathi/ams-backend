80d4ff466c065e03d10047f8d59d10787bdad809
Archisman Tripathi <archismantripathi@gmail.com>
2023-05-20 14:19:26 +0530

    fix1

--- src/api/extension/extension.module.ts
+++ src/api/extension/extension.module.ts
@@ -1,14 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ExtensionService } from './extension.service';
 import { ExtensionController } from './extension.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DeviceSchema } from 'src/models/device.model';
+import { ExtensionRepository } from './extension.repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Device', schema: DeviceSchema }]),
   ],
   controllers: [ExtensionController],
-  providers: [ExtensionService]
+  providers: [ExtensionService, ExtensionRepository],
 })
 export class ExtensionModule {}
